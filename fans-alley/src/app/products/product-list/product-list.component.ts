7433f116547301afe890b186c8a29dc12cf1a03d
kostovm <kostov543@gmail.com>
2024-12-16 08:52:14 +0200

    Added softUni practice server
    I still need to add seeded data but will do it later.

--- fans-alley/src/app/products/product-list/product-list.component.ts
+++ fans-alley/src/app/products/product-list/product-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+
+  constructor(private api: ApiService){}
+
+  ngOnInit(): void {
+    this.api.getProducts().subscribe((product)=> {
+      console.log(product)
+    })
+    
+  }
 
 }
